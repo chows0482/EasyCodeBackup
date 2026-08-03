a37e4d1101c839d2d2ab297e4e586ba0613db667
chows0482 <chows0482@wrdsb.ca>
2026-08-03 13:59:43 -0400

    Fix path to worker redirect URI in Dropbox OAuth flow.

--- cloudflare-worker/worker.js
+++ cloudflare-worker/worker.js
@@ -10,27 +10,14 @@ export default {
         return new Response("Authorization code missing from Dropbox.", { status: 400 });
       }
 
-      try {
-        const fullyDecodedState = decodeURIComponent(decodeURIComponent(state));
-        
-        let cleanState = fullyDecodedState;
-        if (cleanState.includes("vscode://")) {
-          cleanState = cleanState.substring(cleanState.indexOf("vscode://"));
-        } else if (cleanState.includes("vscode-insiders://")) {
-          cleanState = cleanState.substring(cleanState.indexOf("vscode-insiders://"));
-        }
+      const baseScheme = state === "insiders"
+        ? "vscode-insiders://chows0482.easy-code-backup/dropbox" 
+        : "vscode://chows0482.easy-code-backup/dropbox";
 
-        const targetUri = new URL(cleanState);
-        targetUri.searchParams.set("code", code);
+      const targetUri = new URL(baseScheme);
+      targetUri.searchParams.set("code", code);
 
-        return Response.redirect(targetUri.toString(), 302);
-      } catch (err) {
-        console.error("State extraction crash:", err);
-      }
-
-      const fallbackUri = new URL("vscode-insiders://chows0482.easy-code-backup/dropbox");
-      fallbackUri.searchParams.set("code", code);
-      return Response.redirect(fallbackUri.toString(), 302);
+      return Response.redirect(targetUri.toString(), 302);
     }
 
     return new Response("Not Found", { status: 404 });
